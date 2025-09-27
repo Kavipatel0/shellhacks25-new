fc3c1188d817ec5d7138de2f01f52011dc55e780
Stanley Ke <kestanley101@gmail.com>
2025-09-27 09:43:30 -0400

    feat: commit viewer

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,28 @@
 import "./App.css";
 import FlowGraph from "./components/FlowGraph";
+import CommitViewer from "./components/CommitViewer";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="w-screen h-screen p-0 m-0">
-      <FlowGraph />
-    </div>
+    <BrowserRouter>
+      <div className="w-screen h-screen p-0 m-0 flex flex-col">
+        <nav className="p-4 bg-gray-900 text-white flex gap-4">
+          <Link to="/" className="underline">
+            Flow Graph
+          </Link>
+          <Link to="/commit-viewer" className="underline">
+            Commit Viewer
+          </Link>
+        </nav>
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<FlowGraph />} />
+            <Route path="/commit-viewer" element={<CommitViewer />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
